27ffd5d7c74aa5ac12d64684b025662d922223f6
Abdullah B. Khudhair <abdullabasim91@gmail.com>
2023-04-22 02:27:34 +0300

    Update apiFetch.py

--- attendanceApp/apiFetch.py
+++ attendanceApp/apiFetch.py
@@ -9,8 +9,8 @@ class ApiFetch():
     def fetchApiAttendance(serializer):
         try:
 
-            attendance_response = requests.get(os.environ.get('API_URL'), params=serializer.data,
-                                               verify=False)
+            attendance_response = requests.get(os.environ.get('API_URL'), params=serializer.data,timeout=3000, verify=False)
+                                              
 
             if attendance_response.status_code == 200:
                 return (attendance_response.json())
